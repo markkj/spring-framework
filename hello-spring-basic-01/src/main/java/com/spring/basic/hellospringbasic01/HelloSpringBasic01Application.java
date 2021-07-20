2181b5de142f622d0b132bbfc2983c86a7269836
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-20 22:01:11 +0700

    first

---
+++ hello-spring-basic-01/src/main/java/com/spring/basic/hellospringbasic01/HelloSpringBasic01Application.java
@@ -0,0 +1,13 @@
+package com.spring.basic.hellospringbasic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBasic01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBasic01Application.class, args);
+	}
+
+}
