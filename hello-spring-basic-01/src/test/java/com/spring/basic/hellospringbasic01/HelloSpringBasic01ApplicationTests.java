79b7a7b94c013c68ad1dc8b4ddcb91f5cf6c056f
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-22 15:41:48 +0700

    unit test with Spring

--- hello-spring-basic-01/src/test/java/com/spring/basic/hellospringbasic01/HelloSpringBasic01ApplicationTests.java
+++ hello-spring-basic-01/src/test/java/com/spring/basic/hellospringbasic01/HelloSpringBasic01ApplicationTests.java
@@ -1,7 +1,10 @@
 package com.spring.basic.hellospringbasic01;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class HelloSpringBasic01ApplicationTests {
---
+++ hello-spring-basic-01/src/test/java/com/spring/basic/hellospringbasic01/basic/BinarySearchTest.java
@@ -0,0 +1,27 @@
+package com.spring.basic.hellospringbasic01.basic;
+
+import com.spring.basic.hellospringbasic01.HelloSpringBasic01Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ContextConfiguration(classes = HelloSpringBasic01Application.class)
+public class BinarySearchTest {
+
+    @Autowired
+    BinarySearch binarySearch;
+
+    @Test
+    public void testBasicScenario(){
+        int result = binarySearch.binarySearch(new int[]{},4);
+        assertEquals(2,result);
+
+    }
+
+}
+
