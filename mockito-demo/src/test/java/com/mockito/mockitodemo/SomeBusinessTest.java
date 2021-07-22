6f2032b335e47d56c24807e8d8b8cf291730c06b
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-22 15:15:56 +0700

    test with mock

--- mockito-demo/src/test/java/com/mockito/mockitodemo/SomeBusinessTest.java
+++ mockito-demo/src/test/java/com/mockito/mockitodemo/SomeBusinessTest.java
@@ -3,14 +3,29 @@ package com.mockito.mockitodemo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SomeBusinessTest {
 
     @Test
     public void testFindTheGreatestFromAllData(){
+
+
         SomeBusiness business = new SomeBusiness(new DataServiceStub());
         int number = business.findTheGreatestFromAllData();
         assertEquals(24,number);
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllDataWithMock(){
+        DataService dataService = mock(DataService.class);
+        when(dataService.retrieveAllData()).thenReturn(new int[]{24,6,15});
+
+        SomeBusiness business = new SomeBusiness(dataService);
+        int number = business.findTheGreatestFromAllData();
+        assertEquals(24,number);
+
     }
 }
 
