4bd4302eb0464960c8fb6bebf8c001e5039737ed
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-22 17:06:55 +0700

    start with spring-boot

---
+++ spring-boot-demo/src/main/java/com/springboot/springbootdemo/book/BookController.java
@@ -0,0 +1,18 @@
+package com.springboot.springbootdemo.book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBook(){
+        return Arrays.asList(
+                new Book(1l,"Spring Boot Demo","Johny Tu")
+        );
+    }
+}
