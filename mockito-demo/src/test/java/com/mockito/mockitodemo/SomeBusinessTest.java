bbdf340c8ea5cd27a86e8248d1c72f9a7e16481f
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-22 15:22:36 +0700

    test with @Mock and @InjectMocks

--- mockito-demo/src/test/java/com/mockito/mockitodemo/SomeBusinessTest.java
+++ mockito-demo/src/test/java/com/mockito/mockitodemo/SomeBusinessTest.java
@@ -1,30 +1,48 @@
 package com.mockito.mockitodemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessTest {
 
+    @Mock
+    DataService dataService;
+
+    @InjectMocks
+    SomeBusiness business;
+
     @Test
-    public void testFindTheGreatestFromAllData(){
-
-
-        SomeBusiness business = new SomeBusiness(new DataServiceStub());
+    public void testFindTheGreatestFromAllDataWithStub(){
+        business = new SomeBusiness(new DataServiceStub());
         int number = business.findTheGreatestFromAllData();
         assertEquals(24,number);
     }
 
     @Test
     public void testFindTheGreatestFromAllDataWithMock(){
-        DataService dataService = mock(DataService.class);
         when(dataService.retrieveAllData()).thenReturn(new int[]{24,6,15});
 
-        SomeBusiness business = new SomeBusiness(dataService);
         int number = business.findTheGreatestFromAllData();
         assertEquals(24,number);
+
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllDataWithMock_NoValue(){
+        when(dataService.retrieveAllData()).thenReturn(new int[]{});
+
+        int number = business.findTheGreatestFromAllData();
+        assertEquals(Integer.MIN_VALUE,number);
 
     }
 }
