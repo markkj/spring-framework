2d82100404a3d24c778c0b6aa87eb0809bec6737
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-07-22 14:29:26 +0700

    understanding before after each,class

--- junit-5-test/test/com/junit5/MyMathTest.java
+++ junit-5-test/test/com/junit5/MyMathTest.java
@@ -1,14 +1,35 @@
 package com.junit5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyMathTest {
 
+    @BeforeEach
+    public void before(){
+        System.out.println("before");
+    }
+
+    @AfterEach
+    public void after(){
+        System.out.println("after");
+    }
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("beforeClass");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("afterClass");
+    }
+
     @Test
     public void sumWith3Number(){
         MyMath m = new MyMath();
+        System.out.println("test1");
         int sum = m.sum(new int[]{1,2,3});
         assertEquals(6,sum);
     }
@@ -16,6 +37,7 @@ public class MyMathTest {
     @Test
     public void sumWith1Number(){
         MyMath m = new MyMath();
+        System.out.println("test2");
         int sum = m.sum(new int[]{3});
         assertEquals(3,sum);
     }
