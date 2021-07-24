64008bfba529dcf67c590b646367e1b518c2aea2
markkj <electormark@gmail.com>
2021-07-24 20:12:48 +0700

    first webapplication with tomcat and servlet

---
+++ webapplication/src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,26 @@
+package webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("My first web application");
+        out.println("</body>");
+        out.println("</head>");
+    }
+}
